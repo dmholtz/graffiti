dab8bbfb6415af37ebac0630250f814604f4ce4f
dmholtz <56723830+dmholtz@users.noreply.github.com>
2022-08-03 20:39:49 +0200

    Update Graph interface documentation

--- graph/graph.go
+++ graph/graph.go
@@ -5,7 +5,8 @@ type NodeId = int
 
 // Simplest capability description of an outgoing edge without any annotations such as weight.
 type IHalfEdge interface {
-	To() NodeId // returns the nodeId to which the head of this half edge points to
+	// To() returns the head of the edge, i.e. the node id to which this edge points.
+	To() NodeId
 }
 
 // The weight of an edge can be of any number type that supports addition and subtraction.
@@ -13,16 +14,25 @@ type Weight interface {
 	int | float64
 }
 
-// A weighted half edge combines the capabilities of an unweighted half edge and a query to retrieve the weight.
+// Capabilities description of weighted half edges
 type IWeightedHalfEdge[W Weight] interface {
-	IHalfEdge  // capabilities of an unweighted half edge
-	Weight() W // returns the weight of the edge
+	// IWeightedHalfEdge inherits all capabilities of IHalfEdge.
+	IHalfEdge
+	// Weight() returns the weight of type W associated with this edge.
+	Weight() W
 }
 
 // Generic interface of a graph
 type Graph[N any, E IHalfEdge] interface {
+	// NodeCount() returns the number of nodes in the graph.
 	NodeCount() int
+	// EdgeCount() returns the number of edges in the graph.
 	EdgeCount() int
+	// GetNode(id) returns the node n of the graph, which is uniquely identified ID 'id'.
+	// The method panics iff the graph does not contain a node with ID 'id'.
 	GetNode(id NodeId) N
+	// GetHalfEdgesFrom(id) returns a slice of edges, which leave the node with ID=id.
+	// If the node referred by 'id' does not have any leaving edges, an empty slice is returned.
+	// The method panics iff the graph does not contain a node with ID 'id'.
 	GetHalfEdgesFrom(id NodeId) []E
 }
