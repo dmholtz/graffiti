fe0c9f8ad4dcaf84de06da05be2c9a4fbc1bd748
dmholtz <56723830+dmholtz@users.noreply.github.com>
2022-08-05 23:04:11 +0200

    Add test infrastructure

---
+++ algorithms/shortest_path/dijkstra_test.go
@@ -0,0 +1,35 @@
+package shortest_path
+
+import (
+	"encoding/gob"
+	"fmt"
+	"os"
+	"testing"
+
+	g "github.com/dmholtz/graffiti/graph"
+)
+
+const defaultGraphFile = "testdata/geo_graph_7k.gob"
+
+func loadGraph(filename string) g.Graph[g.GeoPoint, g.WeightedHalfEdge[int]] {
+	var aag g.AdjacencyArrayGraph[g.GeoPoint, g.WeightedHalfEdge[int]]
+
+	gobGraphFile, err := os.Open(filename)
+	defer gobGraphFile.Close()
+	if err != nil {
+		panic(fmt.Sprintf("Error while reading .gob file '%s'", filename))
+	}
+
+	dec := gob.NewDecoder(gobGraphFile)
+	err = dec.Decode(&aag)
+	if err != nil {
+		panic(fmt.Sprintf("Error while decoding .gob file '%s", filename))
+	}
+
+	return &aag
+}
+
+func TestDijkstra(t *testing.T) {
+	aag := loadGraph(defaultGraphFile)
+	fmt.Println(aag.NodeCount())
+}
