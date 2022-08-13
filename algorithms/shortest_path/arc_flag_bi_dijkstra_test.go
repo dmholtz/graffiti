40281c45757385c9b4aafe268961946fd48c4ec0
dmholtz <56723830+dmholtz@users.noreply.github.com>
2022-08-13 22:21:36 +0200

    Implement bi-ArcFlagDijkstra

---
+++ algorithms/shortest_path/arc_flag_bi_dijkstra_test.go
@@ -0,0 +1,36 @@
+package shortest_path_test
+
+import (
+	"math"
+	"math/rand"
+	"testing"
+
+	sp "github.com/dmholtz/graffiti/algorithms/shortest_path"
+	g "github.com/dmholtz/graffiti/graph"
+)
+
+// Differential testing: Compare the output of bidirectional ArcFlagDijkstra with unidirectional ArcFlagDijkstra
+func TestBidirectionalArcFlagDijkstra(t *testing.T) {
+	faag := loadAdjacencyArrayFromGob[g.PartGeoPoint, g.FlaggedHalfEdge[int, uint64]](arcflag64) // faag is a undirected graph
+
+	t.Logf("Compare %d random searches of bidirectional Dijkstra against textbook Dijkstra.\n", NUMBER_OF_RANDOM_TESTS)
+	biPqPops, uniPqPops := 0, 0
+	for i := 0; i < NUMBER_OF_RANDOM_TESTS; i++ {
+		source := rand.Intn(faag.NodeCount())
+		target := rand.Intn(faag.NodeCount())
+
+		biResult := sp.BidirectionalArcFlagDijkstra[g.PartGeoPoint, g.FlaggedHalfEdge[int, uint64]](faag, faag, source, target, false, math.MaxInt)
+		uniResult := sp.ArcFlagDijkstra[g.PartGeoPoint, g.FlaggedHalfEdge[int, uint64], int](faag, source, target, false)
+
+		if biResult.Length != uniResult.Length {
+			t.Errorf("[Path(source=%d, target=%d)]: Different lengths found: Bi-ArcFlagDijkstra=%d, ArcFlagDijkstra=%d\n", source, target, biResult.Length, uniResult.Length)
+			return
+		}
+
+		// maintain PQ-pops as performance indicators
+		biPqPops += biResult.PqPops
+		uniPqPops += uniResult.PqPops
+	}
+	biPqPops, uniPqPops = biPqPops/NUMBER_OF_RANDOM_TESTS, uniPqPops/NUMBER_OF_RANDOM_TESTS
+	t.Logf("Avgerage number of PQ.Pop() operations: %d (Bi-ArcFlagDijkstra), %d (ArcFlagDijkstra)\n", biPqPops, uniPqPops)
+}
---
+++ algorithms/shortest_path/shortest_path_test.go
@@ -0,0 +1,36 @@
+package shortest_path_test
+
+import (
+	"encoding/gob"
+	"fmt"
+	"os"
+
+	g "github.com/dmholtz/graffiti/graph"
+)
+
+// Package shortest_path_test contains utility functions and testing constants
+
+// Constants
+const NUMBER_OF_RANDOM_TESTS = 2000
+
+// Path to .gob files
+const arcflag64 = "testdata/geograph_arcflag_64_7k.gob"
+
+// Deserializer
+func loadAdjacencyArrayFromGob[N any, E g.IHalfEdge](filename string) *g.AdjacencyArrayGraph[N, E] {
+	var faag g.AdjacencyArrayGraph[N, E]
+
+	gobGraphFile, err := os.Open(filename)
+	defer gobGraphFile.Close()
+	if err != nil {
+		panic(fmt.Sprintf("Error while reading .gob file '%s'", filename))
+	}
+
+	dec := gob.NewDecoder(gobGraphFile)
+	err = dec.Decode(&faag)
+	if err != nil {
+		panic(fmt.Sprintf("Error while decoding .gob file '%s", filename))
+	}
+
+	return &faag
+}
