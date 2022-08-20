d50bab85cd1f92dff36514b49663558d22eecd38
dmholtz <56723830+dmholtz@users.noreply.github.com>
2022-08-20 22:24:19 +0200

    Add DijkstraRouter and BiDijkstraRouter

--- algorithms/shortest_path/dijkstra.go
+++ algorithms/shortest_path/dijkstra.go
@@ -6,8 +6,72 @@ import (
 	g "github.com/dmholtz/graffiti/graph"
 )
 
+type DijkstraRouter[N any, E g.IWeightedHalfEdge[W], W g.Weight] struct {
+	Graph g.Graph[N, E]
+}
+
+// String implements fmt.Stringer
+func (r DijkstraRouter[N, E, W]) String() string {
+	return "Dijkstra"
+}
+
 // Efficient implementation of Dijkstra's Algorithm for finding the shortest path between a source and a target node in the graph.
 // Implementation is based on a priority queue.
+func (r DijkstraRouter[N, E, W]) Route(source, target g.NodeId, recordSearchSpace bool) ShortestPathResult[W] {
+	var searchSpace []g.NodeId = nil
+	if recordSearchSpace {
+		searchSpace = make([]g.NodeId, 0)
+	}
+
+	dijkstraItems := make([]*DijkstraPqItem[W], r.Graph.NodeCount(), r.Graph.NodeCount())
+	dijkstraItems[source] = &DijkstraPqItem[W]{Id: source, Priority: 0, Predecessor: -1}
+
+	pq := make(DijkstraPriorityQueue[W], 0)
+	heap.Init(&pq)
+	heap.Push(&pq, dijkstraItems[source])
+
+	pqPops := 0
+	for len(pq) > 0 {
+		currentPqItem := heap.Pop(&pq).(*DijkstraPqItem[W])
+		currentNodeId := currentPqItem.Id
+		pqPops++
+
+		if recordSearchSpace {
+			searchSpace = append(searchSpace, currentNodeId)
+		}
+
+		for _, edge := range r.Graph.GetHalfEdgesFrom(currentNodeId) {
+			successor := edge.To()
+
+			if dijkstraItems[successor] == nil {
+				newPriority := dijkstraItems[currentNodeId].Priority + edge.Weight()
+				pqItem := DijkstraPqItem[W]{Id: successor, Priority: newPriority, Predecessor: currentNodeId}
+				dijkstraItems[successor] = &pqItem
+				heap.Push(&pq, &pqItem)
+			} else {
+				if updatedDistance := dijkstraItems[currentNodeId].Priority + edge.Weight(); updatedDistance < dijkstraItems[successor].Priority {
+					dijkstraItems[successor].Priority = updatedDistance
+					dijkstraItems[successor].Predecessor = currentNodeId
+					heap.Fix(&pq, dijkstraItems[successor].index)
+				}
+			}
+		}
+
+		if currentNodeId == target {
+			break
+		}
+	}
+
+	res := ShortestPathResult[W]{Length: W(-1), Path: make([]g.NodeId, 0), PqPops: pqPops, SearchSpace: searchSpace}
+	if dijkstraItems[target] != nil {
+		res.Length = dijkstraItems[target].Priority
+		for nodeId := target; nodeId != -1; nodeId = dijkstraItems[nodeId].Predecessor {
+			res.Path = append([]int{nodeId}, res.Path...)
+		}
+	}
+	return res
+}
+
 func Dijkstra[N any, E g.IWeightedHalfEdge[W], W g.Weight](graph g.Graph[N, E], source, target g.NodeId, recordSearchSpace bool) ShortestPathResult[W] {
 	var searchSpace []g.NodeId = nil
 	if recordSearchSpace {
--- algorithms/shortest_path/shortest_path_test.go
+++ algorithms/shortest_path/shortest_path_test.go
@@ -3,8 +3,11 @@ package shortest_path_test
 import (
 	"encoding/gob"
 	"fmt"
+	"math/rand"
 	"os"
+	"testing"
 
+	sp "github.com/dmholtz/graffiti/algorithms/shortest_path"
 	g "github.com/dmholtz/graffiti/graph"
 )
 
@@ -34,4 +37,43 @@ func loadAdjacencyArrayFromGob[N any, E g.IHalfEdge](filename string) *g.Adjacen
 	}
 
 	return &faag
+}
+
+// DifferentialTesting compares random searches of the testedRouter with the baselineRouter and asserts same lengths.
+// To obtain deterministic results, the Router's weight type is restricted to int.
+func DifferentialTesting(t *testing.T, testedRouter, baselineRouter sp.Router[int], nodeCount int) {
+	baselineName := "baseline"
+	baselineStringer, ok := baselineRouter.(fmt.Stringer)
+	if ok {
+		baselineName = baselineStringer.String()
+	}
+	testedName := "tested"
+	testedStringer, ok := testedRouter.(fmt.Stringer)
+	if ok {
+		testedName = testedStringer.String()
+	}
+
+	t.Logf("Compare %d random searches of %s with %s.\n", NUMBER_OF_RANDOM_TESTS, testedName, baselineName)
+
+	rand.Seed(1) // normalize the seed
+	testedPqPops, baselinePqPops := 0, 0
+	for i := 0; i < NUMBER_OF_RANDOM_TESTS; i++ {
+		source := rand.Intn(nodeCount)
+		target := rand.Intn(nodeCount)
+
+		testedResult := testedRouter.Route(source, target, false)
+		baselineResult := baselineRouter.Route(source, target, false)
+
+		if testedResult.Length != baselineResult.Length {
+			t.Errorf("[Path(source=%d, target=%d)]: Different lengths found: [%s]=%d, [%s]=%d", source, target, testedName, testedResult.Length, baselineName, baselineResult.Length)
+			return
+		}
+
+		// maintain performance indicators
+		testedPqPops += testedResult.PqPops
+		baselinePqPops += baselineResult.PqPops
+	}
+	testedPqPops = testedPqPops / NUMBER_OF_RANDOM_TESTS
+	baselinePqPops = baselinePqPops / NUMBER_OF_RANDOM_TESTS
+	t.Logf("Avgerage number of Pop() operations on priority queue: %d (%s), %d (%s)", testedPqPops, testedName, baselinePqPops, baselineName)
 }
