8f75eff372d7713a46ba92c424d02bee1f0712ea
dmholtz <56723830+dmholtz@users.noreply.github.com>
2022-08-21 21:40:35 +0200

    Add A-Star routers

--- algorithms/shortest_path/a_star_test.go
+++ algorithms/shortest_path/a_star_test.go
@@ -1,7 +1,6 @@
 package shortest_path_test
 
 import (
-	"math/rand"
 	"testing"
 
 	sp "github.com/dmholtz/graffiti/algorithms/shortest_path"
@@ -16,24 +15,8 @@ func TestAStar(t *testing.T) {
 	// Initialize the heurisitc
 	havHeuristic := h.NewHaversineHeuristic[g.WeightedHalfEdge[int], int](aag)
 
-	t.Logf("Compare %d random searches of A* with Dijkstra's algorithm.\n", NUMBER_OF_RANDOM_TESTS)
-	aStarPqPops, dijkstraPqPops := 0, 0
-	for i := 0; i < NUMBER_OF_RANDOM_TESTS; i++ {
-		source := rand.Intn(aag.NodeCount())
-		target := rand.Intn(aag.NodeCount())
+	testedRouter := sp.AStarRouter[g.GeoPoint, g.WeightedHalfEdge[int], int]{Graph: aag, Heuristic: havHeuristic}
+	baselineRouter := sp.DijkstraRouter[g.GeoPoint, g.WeightedHalfEdge[int], int]{Graph: aag}
 
-		aStarResult := sp.AStar[g.GeoPoint, g.WeightedHalfEdge[int], int](aag, havHeuristic, source, target, false)
-		dijkstraResult := sp.Dijkstra[g.GeoPoint, g.WeightedHalfEdge[int], int](aag, source, target, false)
-
-		if aStarResult.Length != dijkstraResult.Length {
-			t.Errorf("[Path(source=%d, target=%d)]: Different lengths found: A*=%d, Dijkstra=%d\n", source, target, aStarResult.Length, dijkstraResult.Length)
-			return
-		}
-
-		// maintain PQ-pops as performance indicators
-		aStarPqPops += aStarResult.PqPops
-		dijkstraPqPops += dijkstraResult.PqPops
-	}
-	aStarPqPops, dijkstraPqPops = aStarPqPops/NUMBER_OF_RANDOM_TESTS, dijkstraPqPops/NUMBER_OF_RANDOM_TESTS
-	t.Logf("Avgerage number of PQ.Pop() operations: %d (A*), %d (Dijkstra)\n", aStarPqPops, dijkstraPqPops)
+	DifferentialTesting(t, testedRouter, baselineRouter, aag.NodeCount())
 }
--- algorithms/shortest_path/bi_a_star.go
+++ algorithms/shortest_path/bi_a_star.go
@@ -6,9 +6,25 @@ import (
 	g "github.com/dmholtz/graffiti/graph"
 )
 
+// BidirectionalAStarRouter implements the Router interface and provides bidirectional A* search, a lower-bounding variant of Dijkstra's algorithm.
+type BidirectionalAStarRouter[N any, E g.IWeightedHalfEdge[W], W g.Weight] struct {
+	Graph     g.Graph[N, E]
+	Transpose g.Graph[N, E]
+
+	ForwardHeuristic  Heuristic[W]
+	BackwardHeuristic Heuristic[W]
+
+	MaxInitializerValue W
+}
+
+// String implements fmt.Stringer
+func (r BidirectionalAStarRouter[N, E, W]) String() string {
+	return "Bidirectional A-Star"
+}
+
 // Bidirectional implementation of the lower-bounding A* algorithm following the symmetric approach by I. Pohl: "Bi-directional Search", 1971
 // cf. Goldberg et al.: "Computing the Shortest Path: A* Search meets Graph Theory", 2004
-func BidirectionalAStar[N any, E g.IWeightedHalfEdge[W], W g.Weight](graph, transpose g.Graph[N, E], forwardHeuristic, backwardHeuristic Heuristic[W], source, target g.NodeId, recordSearchSpace bool, maxInitializerValue W) ShortestPathResult[W] {
+func (r BidirectionalAStarRouter[N, E, W]) Route(source, target g.NodeId, recordSearchSpace bool) ShortestPathResult[W] {
 	var searchSpace []g.NodeId = nil
 	if recordSearchSpace {
 		searchSpace = make([]g.NodeId, 0)
@@ -19,10 +35,10 @@ func BidirectionalAStar[N any, E g.IWeightedHalfEdge[W], W g.Weight](graph, tran
 		return ShortestPathResult[W]{Length: W(0), Path: []g.NodeId{source}, PqPops: 0, SearchSpace: searchSpace}
 	}
 
-	dijkstraItemsForward := make([]*AStarPqItem[W], graph.NodeCount(), graph.NodeCount())
+	dijkstraItemsForward := make([]*AStarPqItem[W], r.Graph.NodeCount(), r.Graph.NodeCount())
 	dijkstraItemsForward[source] = &AStarPqItem[W]{Id: source, Distance: 0, Priority: 0, Predecessor: -1}
 
-	dijkstraItemsBackward := make([]*AStarPqItem[W], graph.NodeCount(), graph.NodeCount())
+	dijkstraItemsBackward := make([]*AStarPqItem[W], r.Graph.NodeCount(), r.Graph.NodeCount())
 	dijkstraItemsBackward[target] = &AStarPqItem[W]{Id: target, Distance: 0, Priority: 0, Predecessor: -1}
 
 	pqForward := make(AStarPriorityQueue[W], 0)
@@ -33,14 +49,14 @@ func BidirectionalAStar[N any, E g.IWeightedHalfEdge[W], W g.Weight](graph, tran
 	heap.Init(&pqBackward)
 	heap.Push(&pqBackward, dijkstraItemsBackward[target])
 
-	forwardSettled := make([]bool, graph.NodeCount(), graph.NodeCount())
-	backwardSettled := make([]bool, graph.NodeCount(), graph.NodeCount())
+	forwardSettled := make([]bool, r.Graph.NodeCount(), r.Graph.NodeCount())
+	backwardSettled := make([]bool, r.Graph.NodeCount(), r.Graph.NodeCount())
 
-	forwardHeuristic.Init(source, target)
-	backwardHeuristic.Init(target, source)
+	r.ForwardHeuristic.Init(source, target)
+	r.BackwardHeuristic.Init(target, source)
 
 	// Once the algorithm terminates, mu contains the shortest path distance between source and target.
-	mu := maxInitializerValue // initialize with the largest representable number of weight type W
+	mu := r.MaxInitializerValue // initialize with the largest representable number of weight type W
 
 	middleNodeId := -1
 
@@ -65,14 +81,14 @@ func BidirectionalAStar[N any, E g.IWeightedHalfEdge[W], W g.Weight](graph, tran
 		}
 
 		// forward search
-		for _, edge := range graph.GetHalfEdgesFrom(forwardNodeId) {
+		for _, edge := range r.Graph.GetHalfEdgesFrom(forwardNodeId) {
 			successor := edge.To()
 			// improvement by Kwa: An admissible bidirectional staged heuristic search algorithm
-			if mu < maxInitializerValue && dijkstraItemsBackward[successor] != nil && backwardSettled[successor] == true {
+			if mu < r.MaxInitializerValue && dijkstraItemsBackward[successor] != nil && backwardSettled[successor] == true {
 				// improvement by Kwa: An admissible bidirectional staged heuristic search algorithm
 				if dijkstraItemsForward[successor] == nil {
 					newDistance := forwardPqItem.Distance + edge.Weight()
-					newPriority := newDistance + forwardHeuristic.Evaluate(successor)
+					newPriority := newDistance + r.ForwardHeuristic.Evaluate(successor)
 					pqItem := AStarPqItem[W]{Id: successor, Priority: newPriority, Distance: newDistance, Predecessor: forwardNodeId}
 					dijkstraItemsForward[successor] = &pqItem
 					// no put on priority queue
@@ -89,12 +105,12 @@ func BidirectionalAStar[N any, E g.IWeightedHalfEdge[W], W g.Weight](graph, tran
 			}
 			if dijkstraItemsForward[successor] == nil {
 				newDistance := forwardPqItem.Distance + edge.Weight()
-				newPriority := newDistance + forwardHeuristic.Evaluate(successor)
+				newPriority := newDistance + r.ForwardHeuristic.Evaluate(successor)
 				pqItem := AStarPqItem[W]{Id: successor, Priority: newPriority, Distance: newDistance, Predecessor: forwardNodeId}
 				dijkstraItemsForward[successor] = &pqItem
 				heap.Push(&pqForward, &pqItem)
 			} else {
-				if updatedPriority := forwardPqItem.Distance + edge.Weight() + forwardHeuristic.Evaluate(successor); updatedPriority < dijkstraItemsForward[successor].Priority {
+				if updatedPriority := forwardPqItem.Distance + edge.Weight() + r.ForwardHeuristic.Evaluate(successor); updatedPriority < dijkstraItemsForward[successor].Priority {
 					dijkstraItemsForward[successor].Distance = forwardPqItem.Distance + edge.Weight()
 					dijkstraItemsForward[successor].Priority = updatedPriority
 					dijkstraItemsForward[successor].Predecessor = forwardNodeId
@@ -104,7 +120,7 @@ func BidirectionalAStar[N any, E g.IWeightedHalfEdge[W], W g.Weight](graph, tran
 
 			//// heuristic check
 			//ld := Dijkstra[N, E, W](graph, successor, target, false).Length
-			//h := forwardHeuristic.Evaluate(successor)
+			//h := r.ForwardHeuristic.Evaluate(successor)
 			//if ld < h {
 			//	fmt.Printf("Heuristic forward is not admissible: l < h: %d < %d\n", int(ld), int(h))
 			//}
@@ -125,14 +141,14 @@ func BidirectionalAStar[N any, E g.IWeightedHalfEdge[W], W g.Weight](graph, tran
 		}
 
 		// backward search
-		for _, edge := range transpose.GetHalfEdgesFrom(backwardNodeId) {
+		for _, edge := range r.Transpose.GetHalfEdgesFrom(backwardNodeId) {
 			successor := edge.To()
 			// improvement by Kwa: An admissible bidirectional staged heuristic search algorithm
-			if mu < maxInitializerValue && dijkstraItemsForward[successor] != nil && forwardSettled[successor] == true {
+			if mu < r.MaxInitializerValue && dijkstraItemsForward[successor] != nil && forwardSettled[successor] == true {
 				// improvement by Kwa: An admissible bidirectional staged heuristic search algorithm
 				if dijkstraItemsBackward[successor] == nil {
 					newDistance := backwardPqItem.Distance + edge.Weight()
-					newPriority := newDistance + backwardHeuristic.Evaluate(successor)
+					newPriority := newDistance + r.BackwardHeuristic.Evaluate(successor)
 					pqItem := AStarPqItem[W]{Id: successor, Priority: newPriority, Distance: newDistance, Predecessor: backwardNodeId}
 					dijkstraItemsBackward[successor] = &pqItem
 					// no put on priority queue
@@ -149,12 +165,12 @@ func BidirectionalAStar[N any, E g.IWeightedHalfEdge[W], W g.Weight](graph, tran
 			}
 			if dijkstraItemsBackward[successor] == nil {
 				newDistance := backwardPqItem.Distance + edge.Weight()
-				newPriority := newDistance + backwardHeuristic.Evaluate(successor)
+				newPriority := newDistance + r.BackwardHeuristic.Evaluate(successor)
 				pqItem := AStarPqItem[W]{Id: successor, Priority: newPriority, Distance: newDistance, Predecessor: backwardNodeId}
 				dijkstraItemsBackward[successor] = &pqItem
 				heap.Push(&pqBackward, &pqItem)
 			} else {
-				if updatedPriority := dijkstraItemsBackward[backwardNodeId].Distance + edge.Weight() + backwardHeuristic.Evaluate(successor); updatedPriority < dijkstraItemsBackward[successor].Priority {
+				if updatedPriority := dijkstraItemsBackward[backwardNodeId].Distance + edge.Weight() + r.BackwardHeuristic.Evaluate(successor); updatedPriority < dijkstraItemsBackward[successor].Priority {
 					dijkstraItemsBackward[successor].Distance = backwardPqItem.Distance + edge.Weight()
 					dijkstraItemsBackward[successor].Priority = updatedPriority
 					dijkstraItemsBackward[successor].Predecessor = backwardNodeId
@@ -175,7 +191,7 @@ func BidirectionalAStar[N any, E g.IWeightedHalfEdge[W], W g.Weight](graph, tran
 	res := ShortestPathResult[W]{Length: W(-1), Path: make([]g.NodeId, 0), PqPops: pqPops, SearchSpace: searchSpace}
 
 	// check if path exists
-	if mu < maxInitializerValue {
+	if mu < r.MaxInitializerValue {
 		res.Length = mu
 		// sanity check: length == dijkstraItemsForward[middleNodeId].priority + dijkstraItemsBackward[middleNodeId].priority
 		if dijkstraItemsForward[middleNodeId] != nil && dijkstraItemsBackward[middleNodeId] != nil {
