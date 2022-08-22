ed723af87e25398e1710d932378b4799d47c6bc3
dmholtz <56723830+dmholtz@users.noreply.github.com>
2022-08-22 21:03:21 +0200

    Add benchmarks package

---
+++ benchmarks/benchmark.go
@@ -0,0 +1,41 @@
+package benchmarks
+
+// Summary summarizes the result of a benchmark.
+type Summary struct {
+	Runs   int     // number of executions
+	Time   float64 // average execution time [ms]
+	PqPops int     // average number of Pop() operations on priority queue
+}
+
+// BenchmarkResult reports details of a benchmark, i.e. the distribution of runtimes and pq-Pops.
+type BenchmarkResult struct {
+	TimeDistribution   []float64 `json:"times"`
+	PqPopsDistribution []int     `json:"pq-pops"`
+}
+
+// Add adds a new observation to the benchmark.
+func (br *BenchmarkResult) Add(time float64, pqPops int) {
+	br.TimeDistribution = append(br.TimeDistribution, time)
+	br.PqPopsDistribution = append(br.PqPopsDistribution, pqPops)
+}
+
+// Summarize builds a summary of the benchmark.
+func (br BenchmarkResult) Summarize() Summary {
+	runs := len(br.TimeDistribution)
+	time := mean(br.TimeDistribution)
+	pqPops := mean(br.PqPopsDistribution)
+	return Summary{Runs: runs, Time: time, PqPops: pqPops}
+}
+
+// mean computest the mean of a slice.
+func mean[N int | float64](slice []N) N {
+	if len(slice) == 0 {
+		return N(0)
+	} else {
+		cum := N(0)
+		for _, sample := range slice {
+			cum += sample
+		}
+		return cum / N(len(slice))
+	}
+}
---
+++ benchmarks/benchmarker.go
@@ -0,0 +1,33 @@
+package benchmarks
+
+import (
+	"math/rand"
+	"time"
+
+	sp "github.com/dmholtz/graffiti/algorithms/shortest_path"
+	g "github.com/dmholtz/graffiti/graph"
+)
+
+const DEFAULT_SEED = 314159265359
+
+type Benchmarker[W g.Weight] struct {
+	NodeRange g.NodeId
+	Router    sp.Router[W]
+	Result    BenchmarkResult
+}
+
+func (b Benchmarker[W]) Run(n int) Summary {
+	rand.Seed(DEFAULT_SEED)
+
+	for i := 0; i < n; i++ {
+		source := rand.Intn(b.NodeRange)
+		target := rand.Intn(b.NodeRange)
+
+		start := time.Now()
+		routingResult := b.Router.Route(source, target, false)
+		time := float64(time.Since(start)) / 1000000 // ms
+
+		b.Result.Add(time, routingResult.PqPops)
+	}
+	return b.Result.Summarize()
+}
